7f0b14c8f94ea5b3b1cae9117be2bbab0b5993b6
Muneer Girnari <muneeragirnari@gmail.com>
2023-07-25 00:37:35 +0530

    solved script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+    let a = 0; b = 1;
+    for(let i=0;i<=n-2;i++){
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 }
 
 module.exports = fibonacci;

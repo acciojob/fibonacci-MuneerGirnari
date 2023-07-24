9a182a28b9b917b6b4bd41d512281d5e04460255
Muneer Girnari <muneeragirnari@gmail.com>
2023-07-25 00:41:36 +0530

    solved script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
 // your code here
     let a = 0; b = 1;
-    for(let i=0;i<=n-2;i++){
+    for(let i=1;i<=n-2;i++){
         let temp = b;
         b = a + b;
         a = temp;
